da7c3dbf2531f0ea1e8c9911e956633b78fd7880
Otroboy2d <notromenjsjsjs@gmail.com>
2026-03-02 12:41:55 -0600

    feat: añadir conexion a la base de datos con Sequelize

---
+++ database.js
@@ -0,0 +1,11 @@
+
+
+import { Sequelize } from "sequelize";
+
+const sequelize=new Sequelize("prograweb2","root","",{
+    host:"localhost",
+    dialect:"mysql",
+    port:3306
+});
+
+export default sequelize;
